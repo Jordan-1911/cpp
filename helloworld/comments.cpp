db7077446b8f773b37e89108da9c3cf512b21d43
jordan-1911 <jordanwashburn95@gmail.com>
2023-07-13 02:11:33 -0600

    comments added

---
+++ helloworld/comments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/**
+ * This is 
+ * a 
+ * multi line 
+ * comment
+ * 
+*/
+
+
+int main()
+{   // This is a comment line
+    std::cout << "This program details how to make comments in C++";
+    return 0;
+}
