df6a787a85542a6c14fc71a610234f0d56059d76
jordan-1911 <jordanwashburn95@gmail.com>
2023-07-13 22:57:52 -0600

    initialization methods added

--- helloworld/objects/objects.cpp
+++ helloworld/objects/objects.cpp
@@ -13,6 +13,19 @@ int main() {
 
     // variable width now has value 5
 
+    // INITIALIZATION
+    // - one of the downsides of declaring a var and then assigning it
+    // is it takes two lines
+    // - we can achieve the same result in one line with initialization
+    int a;        // no initializer (default initialization)
+    int b = 5;    // initializer after equals sign (copy initialization)
+    int c(6);     // initializer in parenthesis (direct initialization)
+
+    // List initialization methods in (C++)
+    int d{7};     // initializer in braces (direct list initialization)
+    int e = {8};  // initializer in braces after equals sign (copy list initialization)
+    int f{};      // initializer is empty braces (empty initialization)
     
+    return 0;
 }
 
