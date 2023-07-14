70ba7c4c61a263b74ed5c88160080eda72ed8457
jordan-1911 <jordanwashburn95@gmail.com>
2023-07-13 22:49:19 -0600

    assignment and declaration explained

---
+++ helloworld/objects/objects.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    // This will cover the basics of variable assignment
+    // and initialization
+
+    int x;  // define a variable named x
+    int y, z;  // define two integer variables, named y and z
+
+    // VARIABLE ASSIGNMENT
+    int width;  // define an integer variable named width
+    width = 5;  // copy assignment of value 5 into variable width
+
+    // variable width now has value 5
+
+    
+}
+
