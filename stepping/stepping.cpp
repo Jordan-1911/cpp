9a56ac41daf0d884c0f1e571903d0e7658cc9f07
jordan-1911 <jordanwashburn95@gmail.com>
2023-07-22 01:03:07 -0600

    step in, step out, step over explained

--- stepping/stepping.cpp
+++ stepping/stepping.cpp
@@ -3,12 +3,15 @@
  * 
  * Stepping
  * 
- * Step into
+ * STEP INTO
  * - the step into command executes the next statement in the normal execution of the program so we can examine the program's state using the debugger. If the statement being executed contains a function call, step into causes the program to jump to the top of the function being called, where it will pause.
+ * - in VS Code can be chosen using F11
  * 
- * The program below demonstrates this concept.
+ * STEP OVER
+ * - executes the next statement in the normal execution path. However, whereas step into will enter function calls and execute them line by line, step over will execute an entire function without stopping and return control to you after the function has been executed.
  * 
- * Step into in VS Code can be chosen using F11
+ * STEP OUT
+ * Step out does not just execute the next line of code. Instead, it executes all remaining code in the function being executed, and then returns control to you when the function has returned. 
 */
 
 #include <iostream>
