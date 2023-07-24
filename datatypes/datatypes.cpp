067c6ba91164b715d69e1735434b6a03eb136b07
jordan-1911 <jordanwashburn95@gmail.com>
2023-07-24 00:20:49 -0600

    notes added about data types in C++

--- datatypes/datatypes.cpp
+++ datatypes/datatypes.cpp
@@ -25,5 +25,40 @@
  * Address 1 ---- 10010111
  * Address 0 ---- 01101001
  * 
+ * - because all data on a computer is just a sequence of bits, we use a data type to tell the compiler how to interpret the contents of memory in some meaningful way.
+ * - When you give an object a value, the compiler and CPU take care of encoding your value into the appropriate sequence of bits for that data type.
+ * - For example, when you assign an integer object the value of 65, that value is converted to the sequence of bits 0100 0001 and sotred in the memory assigned to the objecf.
+ * 
+ * 
+ * FUNDAMENTAL DATA TYPES
+ * - AKA basic types
+ * - AKA primitive types
+ * - AKA built-in types
+ * 
+ * float ---- floating point ---- number with a fractional part ---- 3.1415
+ * double ---- floating point ---- number with a fractional part ---- 3.1415
+ * long double ---- floating point ---- number with a fractional part ---- 3.1415
+ * bool ---- Integral (Boolean) ---- true or false ---- true
+ * char ---- Integral (Character) ---- a single character of text ---- 'c'
+ * wchar_t ---- Integral (Character) ---- a single character of text ---- 'c'
+ * char8_t (C++ 20) Integral (Character) ---- a single character of text ---- 'c'
+ * char16_t (C++11) ---- Integral (Character) ---- a single character of text ---- 'c'
+ * char32_t (C++11) ---- Integral (Character) ---- a single character of text ---- 'c'
+ * 
+ * short int ---- Integral (Integer) ---- positive and negative whole numbers, including 0 ---- 64
+ * int ---- Integral (Integer) ---- positive and negative whole numbers, including 0 ---- 64
+ * long int ---- Integral (Integer) ---- positive and negative whole numbers, including 0 ---- 64
+ * long long int (C++11) ---- Integral (Integer) ---- positive and negative whole numbers, including 0 ---- 64
+ * 
+ * std::nullptr_t (C++11) ---- Null Pointer ---- a null pointer ---- nullptr
+ * 
+ * void ---- Void ---- no type ---- n/a
+ * 
+ * - most modern programming languages include a fundamental *string* type
+ * 
+ * - The _t suffix
+ * -- many of the types defined in newer versions of C++ (e.g. std::nullptr_t) use a _t suffix. This simply means "type" and its a commen nomenclature applied to modern types
+ * 
+ * 
  * 
  */
