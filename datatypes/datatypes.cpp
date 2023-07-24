115bec2fb279384299ea663862ffbfd196e6be07
jordan-1911 <jordanwashburn95@gmail.com>
2023-07-23 23:57:29 -0600

    data types in C++ 20 explained

---
+++ datatypes/datatypes.cpp
@@ -0,0 +1,29 @@
+/**
+ * INTRO TO DATA TYPES
+ *
+ * - Recap: variables are names for a piece of memory that can be used to store information. Computers have RAM that is available for programs to use. When a variable is defined, a piece of memory is set aside for that variable
+ * 
+ * - BINARY DIGIT
+ * -- the smallest unit of memory is a binary digit or a bit
+ * -- a bit can hold a value of 0 or 1
+ * -- Like a light switch, either the light is OFF (0),
+ * or the light is ON (1)
+ * -- there is no in between. If we were to look at a random segment of memory, all we would see is 011010101100 or some combination thereof
+ * 
+ * - memory is organized into sequential units called memory addresses or addresses for short
+ * -- this is similar to how a street address can be used to find a given house on a street, the memory address allows us to find and access the contents of memory at a particular location
+ * 
+ * - in modern computer architectures, each bit does not get its own unique memory address. This is because the number of memory addresses is limited, and need to access data bit-by-bit is rate. Instead, each memory address holds 1 byte of data.
+ * 
+ * - BYTES
+ * -- a byte is a group of bits that are operated on as a unit. The modern standard is that a byte is comprised of 8 sequential bits.
+ * 
+ * For illustration:
+ * 
+ * Address 3 ---- 11101000
+ * Address 2 ---- 00000000
+ * Address 1 ---- 10010111
+ * Address 0 ---- 01101001
+ * 
+ * 
+ */
